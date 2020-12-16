cc353039bbff65b58049f3a86fb711d832092af3
shivamgupta88 <tsg782@gmail.com>
2020-12-16 19:14:38 +0530

    leat-js

---
+++ app.js
@@ -0,0 +1,8 @@
+// var username = prompt("Give me username")
+// alert("this script works " + username);
+// a way to show inpu t and output
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click",functionclickHandler())
+{
+    click.log("clicked")
+}
