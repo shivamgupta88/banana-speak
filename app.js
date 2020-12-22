70c9c9d5745d6f2edaa3437b49e7f85615cac4ea
shivamgupta88 <tsg782@gmail.com>
2020-12-22 10:17:04 +0530

    till-output

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-// var username = prompt("Give me username")
-// alert("this script works " + username);
-// a way to show inpu t and output
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector('#txt-input')
-{
-    console.log(txtInput)
-}
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked")
 
-})
-function({
-    console.log("input",txtInput.value);
-})
+ 
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
